68addaab0eed30d7b43a89b2534178c8daa442f6
JChangg <cc751@gmail.com>
2016-09-17 01:29:17 +0100

    Upgraded back end to postgresql

--- treg_server/settings.py
+++ treg_server/settings.py
@@ -67,8 +67,12 @@ WSGI_APPLICATION = 'treg_server.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'TREG',
+        'USER': 'TREG_SERVER',
+        'PASSWORD': '>RrS.yf]H\\42JH=a',
+        'HOST': 'localhost',
+        'PORT': '5432'
     }
 }
 
