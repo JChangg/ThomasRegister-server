0a6ea5a9ec12e47d35d6f88dd946c7313626b3ed
JChangg <cc751@gmail.com>
2016-09-18 01:26:29 +0100

    added views to handle web requests

--- register/views.py
+++ register/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from models import Log
+from users.models import Person
 
-# Create your views here.
+
+def index(request):
+    return HttpResponse("Hello, world. You're at the logging index.")
+
+
+def log(request, card_id):
+    registered_person = Person.objects.get(pk=card_id)
+    new_log = Log(person=registered_person)
+    new_log.save()
+    return HttpResponse("Logged %i: %s" % (card_id, registered_person.username))
