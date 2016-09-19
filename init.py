8892c549e99945e9536edc9b725ccf63bc3e6037
JChangg <cc751@gmail.com>
2016-09-19 01:06:58 +0100

    Installed REST api framework

--- init.py
+++ init.py
@@ -1,9 +1,19 @@
 from users import models
 from getpass import getpass
 
-
 print "Initializing database superuser..."
 first_name = raw_input('First name: ')
 last_name = raw_input('Last name: ')
 password = getpass('pw: ')
 
+person = models.Person(
+    first_name=first_name,
+    last_name=last_name,
+    password=password,
+)
+
+user = models.User(
+    person=person,
+    is_staff=True,
+    is_admin=True,
+)
