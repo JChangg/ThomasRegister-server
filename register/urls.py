b7f8189b81552e81059ecd45376ac47d2025e36b
Jamie Chang <cc751@gmail.com>
2016-10-02 21:46:36 +0100

    added the ability to create token using rest api

--- register/urls.py
+++ register/urls.py
@@ -1,8 +1,11 @@
 from django.conf.urls import url
+from rest_framework.authtoken import views as auth_views
+
 
 from . import views
 
 urlpatterns = [
     url(r'^api$', views.register, name='register'),
     url(r'^api/(?P<pk>[0-9]+)$', views.validate, name='register'),
+    url(r'api-token-auth/', auth_views.obtain_auth_token),
 ]
--- register/views.py
+++ register/views.py
@@ -3,8 +3,9 @@ from django.views.decorators.csrf import csrf_exempt
 from register.serializers import LogSerializer
 from users.serializers import CardSerializer
 from users.models import Card
-from rest_framework.decorators import api_view
+from rest_framework.decorators import api_view, permission_classes
 from django.shortcuts import get_object_or_404
+from rest_framework.permissions import IsAuthenticated
 import logging
 
 logger = logging.getLogger(__name__)
@@ -12,6 +13,7 @@ logger = logging.getLogger(__name__)
 
 @csrf_exempt
 @api_view(['GET'])
+@permission_classes((IsAuthenticated,))
 def validate(_, pk):
     card = get_object_or_404(Card, pk=pk)
     card_serializer = CardSerializer(card)
@@ -20,6 +22,7 @@ def validate(_, pk):
 
 @csrf_exempt
 @api_view(['POST'])
+@permission_classes((IsAuthenticated,))
 def register(request):
     log_serializer = LogSerializer(data=request.data)
     if log_serializer.is_valid():
