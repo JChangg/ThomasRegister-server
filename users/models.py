9ec8ce6d978a632df4015e336f3c5d4cccebcc4f
JChangg <cc751@gmail.com>
2016-10-21 21:36:18 +0100

    Removed unused imports

--- users/models.py
+++ users/models.py
@@ -3,7 +3,6 @@ from django.contrib.auth.models import AbstractUser, Group
 from django.db.models.signals import post_save
 from django.dispatch import receiver
 from django.db import models
-import names
 
 
 def get_initials(first_name, middle_names=None, last_name=None):
